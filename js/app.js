3a2ec9c0b1655b36cd22dc7e8af5a24f7eb97348
Erica Bee <ebee511@gmail.com>
2018-07-05 21:53:49 -0700

    Adding functionality to enemy images. Fixing enemy update method.

--- js/app.js
+++ js/app.js
@@ -12,6 +12,13 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = this.x + this.speed * dt;
+
+    // Checks to see if enemy is off page,
+    // If off page, reset position of bug
+    if (this.x > 500) {
+        this.x = 0;
+    };
 };
 
 // Draw the enemy on the screen, required method for game
@@ -33,22 +40,30 @@ var Player = function(x, y, speed) {
 };
 
 Player.prototype.update = function(dt) {
-
+    //Update player position
+    //Check for collide (if player x and y collide with enemy x and y)
+    //Check is player reaches final destination - if so, they won
 };
 
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-Player.prototype.handleInput = function(allowedKeys) {
+Player.prototype.handleInput = function(arrowKey) {
+    //Moves player coordinates 
+};
 
+Player.prototype.resetPlayer = function() {
+    //Brings player back to specific coordinates if they is a collision
+    player.x = 200;
+    player.y = 390;
 };
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
-var enemy1 = new Enemy(0, 310, 10);
-var enemy2 = new Enemy(0, 225, 20);
-var enemy3 = new Enemy(0, 145, 30);
+var enemy1 = new Enemy(0, 145, 80); //bottom
+var enemy2 = new Enemy(0, 225, 100);
+var enemy3 = new Enemy(0, 310, 150); //top
 
 // Place the player object in a variable called player
 var allEnemies = [];
